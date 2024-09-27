71f77f5e8240bc62da6c34f6a684d270102ec846
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2024-09-27 12:49:39 +0200

    Update function

---
+++ Sources/Extensios/ArrayExtension.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  SJDToolBox
+//
+//  Created by Sebby on 27/09/2024.
+//
+
+import Foundation
---
+++ Tests/SJDToolBoxTests/StringExtensionTest.swift
@@ -0,0 +1,17 @@
+//
+//  Test.swift
+//  SJDToolBox
+//
+//  Created by Sebby on 27/09/2024.
+//
+
+import Testing
+@testable import SJDToolBox
+
+struct Test {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
